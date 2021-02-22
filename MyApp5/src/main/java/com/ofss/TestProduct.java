703d89b4414731fe1058cbe7851139d0c90e1028
rohitk4019 <rohitk4019@gmail.com>
2021-02-22 11:11:34 +0530

    "eclipse commit"

---
+++ MyApp5/src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,14 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		System.out.println("Starting the application");
+		Stock st1=new Stock(100, "ORCL", 123.4);
+		System.out.println(st1);
+		System.out.println("=========================");
+				
+
+	}
+
+}
